674d5582534edc3d7104cf3a6ceaec13788f58bb
Yaeno <ing.zhang@gmail.com>
2023-06-08 05:40:40 -0700

    jsfundamental #1

---
+++ javaScriptFundamental1_console.js
@@ -0,0 +1,13 @@
+console.log(3%25)
+console.log(7%28)
+console.log(!(true && false))
+console.log(!true || !false)
+
+let bootcamp;
+bootcamp= "App Academy";
+console.log(bootcamp);
+
+let birthyear = 2012;
+console.log(birthyear);
+
+console.log("academy".indexOf("r"));
